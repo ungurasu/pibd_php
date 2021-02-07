973873dca1b89b9473c36333068a1154a98c0a2f
Ungurasu Ioan-Andrei <ioan.ungurasu@gmail.com>
2021-02-07 22:34:34 +0200

    insert 80% gata

---
+++ pages/insert_action.php
@@ -0,0 +1,50 @@
+<div class="py-5">
+    <?php
+    $bd = DBConnection::getInstance()::getDB();
+    ?>
+
+    <?php
+    // TODO: verificam daca post-ul este gol
+    // TODO: redirectionare
+    // TODO: curatenie
+    printf("<div> Table: %s </div>",$_GET['table']);
+    $columns_querry = $bd->query(sprintf("SHOW COLUMNS FROM %s;",$_GET['table']));
+    foreach($columns_querry as $column) {
+        if ($column['Extra'] != 'auto_increment') {?>
+        <div>
+            <?php printf("%s: %s",$column['Field'],$_POST[$column['Field']])?>
+        </div>
+    <?php
+        }
+    }
+    ?>
+
+    <?php
+    $sql_command = sprintf("INSERT INTO %s (",$_GET['table']);
+    foreach($columns_querry as $column) {
+        if ($column['Extra'] != 'auto_increment') {
+            $sql_command .= sprintf("%s, ", $column['Field']);
+        }
+    }
+    $sql_command = substr($sql_command,0,-2);
+    $sql_command .= ') VALUES (';
+    foreach($columns_querry as $column) {
+        if ($column['Extra'] != 'auto_increment') {
+            $sql_command .= sprintf("'%s', ", $_POST[$column['Field']]);
+        }
+    }
+    $sql_command = substr($sql_command,0,-2);
+    $sql_command .= ');';
+
+    if ($bd->query($sql_command) === TRUE) { ?>
+        <div class="alert alert-success" role="alert">
+            Am inserat cu succes!
+        </div>
+    <?php } else { ?>
+        <div class="alert alert-danger" role="alert">
+            <?php printf("%s", $bd->error)?>
+        </div>
+    <?php
+    }
+    ?>
+</div>
--- pages/insert_form.php
+++ pages/insert_form.php
@@ -7,11 +7,22 @@
         <h1>Insereaza datele in tabelul <?php printf("%s",$_GET['table'])?></h1>
 
         <?php
-        $columns = $bd->query(sprintf("SHOW COLUMNS FROM %s;",$_GET['table']));
-
+        $columns_querry = $bd->query(sprintf("SHOW COLUMNS FROM %s;",$_GET['table']));
         ?>
-        <form action="/" method="get">
-            <>
+        <form action="/?page=insert_action&table=<?php printf("%s",$_GET['table'])?>" method="post">
+            <?php
+            foreach ($columns_querry as $column) {
+                if ($column["Extra"] != 'auto_increment') {?>
+                    <div class="form-group">
+                        <label>Camp: <?php printf("%s",$column["Field"])?></label>
+                        <input type="text" class="form-control" id="<?php printf("%s",$column["Field"])?>" name="<?php printf("%s",$column["Field"])?>" required>
+                        <small class="form-text text-muted">Tip: <?php printf("%s",$column["Type"])?></small>
+                    </div>
+            <?php
+                }
+            }
+            ?>
+            <button type="submit" class="btn btn-primary">Insereaza</button>
         </form>
     <?php } ?>
 </div>
