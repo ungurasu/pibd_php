fa682433c56b2fd552783521ea21af4cc5a7a602
Ungurasu Ioan-Andrei <ioan.ungurasu@gmail.com>
2021-02-15 21:42:10 +0200

    homescreen

--- pages/home.php
+++ pages/home.php
@@ -1,16 +1,28 @@
 <div>
     <h1>Proiect PIBD</h1>
 
-    <table class="table">
+    <table>
         <tbody>
             <tr>
-                <td><img src="images/php.png" class="image-responsive" width="400px"></td>
-                <td><img src="images/bootstrap.png" class="image-responsive" width="400px"></td>
+                <td class="px-2">
+                    <div class="card" style="width: 20vw; height: 50vh;">
+                        <img src="images/php.png" class="imagecard-img-top">
+                        <div class="card-body">
+                            <h2 class="card-title">De ce PHP?</h2>
+                            <p class="card-text">PHP este un scripting language accesibil, bazat pe C si pe C++. Este o solutie clasica pentru web development, folosita la scara larga in ziua de azi.</p>
+                        </div>
+                    </div>
+                </td>
+                <td class="px-2">
+                    <div class="card" style="width: 20vw; height: 50vh;">
+                        <img src="images/bootstrap.png" class="imagecard-img-top">
+                        <div class="card-body">
+                            <h2 class="card-title">De ce Bootstrap?</h2>
+                            <p class="card-text">Bootstrap este un framework open-source, pentru front-end web development. El contine template-uri de CSS si JavaScript.</p>
+                        </div>
+                    </div>
+                </td>
             </tr>
         </tbody>
     </table>
-
-    <h4>test</h4>
-    <h5>test</h5>
-    <h6>test</h6>
 </div>
